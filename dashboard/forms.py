0f880b68ad44356acb82dbf9e8d2840f2102345a
Joost Plattel <jsplattel@gmail.com>
2019-10-16 23:21:23 +0200

    moved a lot of logic towards forms, improved styling

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,17 +1,23 @@
 from django import forms
-from django.forms import ModelForm, ModelChoiceField, ValidationError
+from django.forms import ModelForm, ModelChoiceField, ValidationError, Form
 from dashboard.models import Product, Location, Mutation, Inventory
 
 
 class ProductForm(ModelForm):
     name = forms.CharField(
         widget=forms.TextInput(
-            attrs={"class": "pa2 input-reset ba bg-transparent w-100"}
+            attrs={
+                "placeholder": "Product name",
+                "class": "pa2 input-reset ba bg-transparent w-100",
+            }
         )
     )
     desc = forms.CharField(
         widget=forms.Textarea(
-            attrs={"class": "pa2 input-reset ba bg-transparent w-100"}
+            attrs={
+                "placeholder": "Description..",
+                "class": "pa2 input-reset ba bg-transparent w-100",
+            }
         )
     )
 
@@ -23,12 +29,18 @@ class ProductForm(ModelForm):
 class LocationForm(ModelForm):
     name = forms.CharField(
         widget=forms.TextInput(
-            attrs={"class": "pa2 input-reset ba bg-transparent w-100"}
+            attrs={
+                "placeholder": "Location name",
+                "class": "pa2 input-reset ba bg-transparent w-100",
+            }
         )
     )
     desc = forms.CharField(
         widget=forms.Textarea(
-            attrs={"class": "pa2 input-reset ba bg-transparent w-100"}
+            attrs={
+                "placeholder": "Description..",
+                "class": "pa2 input-reset ba bg-transparent w-100",
+            }
         )
     )
 
@@ -38,11 +50,24 @@ class LocationForm(ModelForm):
 
 
 class MutationForm(ModelForm):
+    amount = forms.FloatField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Amount",
+                "class": "pa2 input-reset ba bg-transparent w-100",
+            }
+        )
+    )
     product = ModelChoiceField(Product.objects.all(), empty_label=None)
     location = ModelChoiceField(Location.objects.all(), empty_label=None)
-    amount = forms.FloatField(widget=forms.NumberInput(attrs={"placeholder": "Amount"}))
     desc = forms.CharField(
-        widget=forms.Textarea(attrs={"placeholder": "Description.."})
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "placeholder": "Description..",
+                "class": "pa2 input-reset ba bg-transparent w-100",
+            }
+        ),
     )
 
     class Meta:
@@ -68,3 +93,67 @@ class MutationForm(ModelForm):
                 )
         else:
             cleaned_data["operation"] = "add"
+
+
+class ShortcutMoveForm(Form):
+    amount = forms.FloatField(
+        widget=forms.NumberInput(
+            attrs={
+                "placeholder": "Amount",
+                "class": "pa2 input-reset ba bg-transparent w-100",
+            }
+        )
+    )
+    product = ModelChoiceField(Product.objects.all(), empty_label=None)
+    location_from = ModelChoiceField(Location.objects.all(), empty_label=None)
+    location_to = ModelChoiceField(Location.objects.all(), empty_label=None)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        amount = cleaned_data.get("amount")
+        product = cleaned_data.get("product")
+        location_from = cleaned_data.get("location_from")
+        location_to = cleaned_data.get("location_to")
+
+        if float(cleaned_data["amount"]) < 0.0:
+            cleaned_data["operation"] = "remove"
+            inventory, created = Inventory.objects.get_or_create(
+                product=product, location=location_from
+            )
+            if inventory.amount + amount < 0:
+                raise ValidationError(
+                    "Insufficient inventory of {} {} at {}".format(
+                        abs(amount), product.name, location_from.name
+                    )
+                )
+        else:
+            cleaned_data["operation"] = "add"
+
+    def save(self):
+        cleaned_data = super().clean()
+
+        # Get data
+        amount = cleaned_data.get("amount")
+        product = cleaned_data.get("product")
+        location_from = cleaned_data.get("location_from")
+        location_to = cleaned_data.get("location_to")
+
+        mutation_from = Mutation(
+            amount=-amount,
+            product=product,
+            location=location_from,
+            operation="remove",
+            desc="Moved {} {} to {}".format(amount, product.name, location_to.name),
+        )
+        mutation_from.save()
+
+        mutation_to = Mutation(
+            amount=amount,
+            product=product,
+            location=location_to,
+            operation="remove",
+            desc="Received {} {} from {}".format(
+                amount, product.name, location_from.name
+            ),
+        )
+        mutation_to.save()
--- dashboard/views_shortcuts.py
+++ dashboard/views_shortcuts.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from dashboard.models import Location, Inventory, Product, Mutation
-from dashboard.forms import MutationForm
+from dashboard.forms import MutationForm, ShortcutMoveForm
 
 
 @login_required
@@ -43,50 +43,26 @@ def shortcut_sales(request):
 @login_required
 def shortcut_move(request):
     if request.method == "POST":
-        mutable_form = request.POST.copy()
-        location_from, location_to = request.POST.getlist("location")
-        location_from = Location.objects.get(id=location_from)
-        location_to = Location.objects.get(id=location_to)
-        product = Product.objects.get(id=mutable_form.get("product"))
-        amount = float(mutable_form["amount"])
+        form = ShortcutMoveForm(request.POST)
 
-        # From
-        mutable_form["amount"] = -amount
-        mutable_form["operation"] = "remove"
-        mutable_form["desc"] = "Moved {} {} to {}".format(
-            amount, product.name, location_to.name
-        )
-        mutable_form["location"] = location_from.id
-        form_from = MutationForm(mutable_form)
-        if form_from.is_valid() is False:
-            messages.add_message(
-                request, messages.ERROR, form_from.non_field_errors().as_text()
-            )
-            return render(request, "shortcuts/shortcut_move.html", {"form": form_from})
-
-        # To
-        mutable_form["amount"] = amount
-        mutable_form["operation"] = "add"
-        mutable_form["desc"] = "Received {} {} from {}".format(
-            amount, product.name, location_from.name
-        )
-        mutable_form["location"] = location_to.id
-        form_to = MutationForm(mutable_form)
-        if not form_to.is_valid():
-            messages.add_message(
-                request, messages.ERROR, form_to.non_field_errors().as_text()
-            )
-            return render(request, "shortcuts/shortcut_move.html", {"form": form_from})
-
-        # If all is valid, save and apply
-        if form_from.is_valid() and form_to.is_valid():
-            mutation_from = form_from.save()
-            mutation_to = form_to.save()
+        # If the form is valid:
+        if form.is_valid():
+            form.save()
+            amount = request.POST.get("amount")
+            product = Product.objects.get(id=request.POST.get("product"))
             messages.add_message(
                 request, messages.SUCCESS, "{} {} moved!".format(amount, product.name)
             )
             return redirect("dashboard")
 
+        # Otherwise return with errors:
+        else:
+            messages.add_message(
+                request, messages.ERROR, form.non_field_errors().as_text()
+            )
+            return render(request, "shortcuts/shortcut_sales.html", {"form": form})
+
+    # When we are just getting the form
     else:
-        form = MutationForm(initial={"amount": 1.0})
+        form = ShortcutMoveForm(initial={"amount": 1, **request.GET.dict()})
         return render(request, "shortcuts/shortcut_move.html", {"form": form})
