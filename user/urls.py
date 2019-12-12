8bbebfac5e6cce162d63596f1be375b3923b8583
Joost Plattel <jsplattel@gmail.com>
2019-12-12 23:00:29 +0100

    Updated static site and fixed some html issues

--- user/urls.py
+++ user/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from organization.invite import OrganizationInvitationBackend
 from user import views
 import notifications.urls
+from django.contrib.auth import views as auth_views
+from django.shortcuts import reverse
 
 urlpatterns = [
     path(
@@ -14,5 +16,7 @@ urlpatterns = [
     path("settings", views.settings, name="user_settings"),
     path("organizations", views.organizations, name="user_organizations"),
     path("notifications", include(notifications.urls, namespace='user')),
-
+    path('password/reset', auth_views.PasswordResetView.as_view(
+        html_email_template_name='registration/password_reset_email_html.html'
+    ), name="password_reset"),
 ]
