730ebd6e3a052e335c4d37ad179cda6e2b3a94dd
Joost Plattel <jsplattel@gmail.com>
2019-12-29 19:43:57 +0100

    Feature improving api docs (#21)
    * now more endpoints in the API
    
    * now more endpoints in the API
    
    * reworked a lot of the apis
    
    * added drf mypy stubs
    
    * formatting

--- api/views.py
+++ api/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets
+from rest_framework import generics, viewsets
 from rest_framework.authtoken.models import Token
-from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.views import APIView, ObtainAuthToken
 from rest_framework.response import Response
+from rest_framework.status import (
+    HTTP_200_OK,
+    HTTP_201_CREATED,
+    HTTP_204_NO_CONTENT,
+    HTTP_400_BAD_REQUEST,
+)
 
 from api.serializers import (
     InventorySerializer,
@@ -10,7 +16,30 @@ from api.serializers import (
     MutationSerializer,
     ProductSerializer,
 )
-from organization.models import Inventory, Location, Mutation, Product
+from organization.models import Inventory, Location, Mutation, Organization, Product
+
+
+def save_serializer_with_organization(serializer, organization):
+    if serializer.instance:  # We are updating
+        status = HTTP_200_OK
+    else:
+        status = HTTP_201_CREATED
+
+    if serializer.is_valid():
+        # Assign the organization
+        serializer.validated_data["organization"] = get_object_or_404(
+            Organization, slug=organization
+        )
+        serializer.save()
+        return Response(serializer.data, status=status)
+    else:
+        return Response(
+            {
+                "message": "An error occured with the operation",
+                "errors": serializer.errors,
+            },
+            status=HTTP_400_BAD_REQUEST,
+        )
 
 
 class ApiAuthorize(ObtainAuthToken):
@@ -30,53 +59,160 @@ class ApiAuthorize(ObtainAuthToken):
         )
 
 
-class ProductViewSet(viewsets.ViewSet):
+class ProductView(generics.ListCreateAPIView):
+
+    serializer_class = ProductSerializer
+
     def list(self, request, organization):
+        """
+        A list of all the products within the organization
+        """
         queryset = Product.objects.for_organization(organization)
-        serializer = ProductSerializer(queryset, many=True)
+        serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
-    def retrieve(self, request, organization, pk=None):
+    def create(self, request, organization):
+        """
+        Create a product within the organization
+        """
+        serializer = self.get_serializer(data=request.data)
+        return save_serializer_with_organization(serializer, organization)
+
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+    serializer_class = ProductSerializer
+
+    def get(self, request, pk, organization):
+        """
+        Get a single product by ID
+        """
         queryset = Product.objects.for_organization(organization)
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = ProductSerializer(user)
+        product = get_object_or_404(Product, pk=pk)
+        serializer = self.get_serializer(product)
         return Response(serializer.data)
 
+    def put(self, request, pk, organization):
+        """
+        Update a single product by ID
+        """
+        queryset = Product.objects.for_organization(organization)
+        product = get_object_or_404(Product, pk=pk)
+        serializer = self.get_serializer(product, data=request.data)
+        return save_serializer_with_organization(serializer, organization)
 
-class LocationViewSet(viewsets.ViewSet):
+    def patch(self, request, pk, organization):
+        """
+        Update a single product by ID
+
+        """
+        queryset = Product.objects.for_organization(organization)
+        product = get_object_or_404(Product, pk=pk)
+        serializer = self.get_serializer(product, data=request.data)
+        return save_serializer_with_organization(serializer, organization)
+
+    def delete(self, request, pk, organization):
+        """
+        Delete a single product by ID
+        """
+        queryset = Product.objects.for_organization(organization)
+        product = get_object_or_404(queryset, pk=pk)
+        product.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
+
+
+class LocationView(generics.ListCreateAPIView):
+
+    serializer_class = LocationSerializer
+
     def list(self, request, organization):
         queryset = Location.objects.for_organization(organization)
-        serializer = LocationSerializer(queryset, many=True)
+        serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
-    def retrieve(self, request, organization, pk=None):
+    def create(self, request, organization):
+        serializer = LocationSerializer(data=request.data)
+        return save_serializer_with_organization(serializer, organization)
+
+
+class LocationDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+    serializer_class = LocationSerializer
+
+    def get(self, request, pk, organization):
+        """
+        Get a single product by ID
+        """
         queryset = Location.objects.for_organization(organization)
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = LocationSerializer(user)
+        location = get_object_or_404(Location, pk=pk)
+        serializer = self.get_serializer(location)
         return Response(serializer.data)
 
+    def put(self, request, pk, organization):
+        """
+        Update a single location by ID
+        """
+        queryset = Location.objects.for_organization(organization)
+        location = get_object_or_404(Location, pk=pk)
+        serializer = self.get_serializer(location, data=request.data)
+        return save_serializer_with_organization(serializer, organization)
 
-class InventoryViewSet(viewsets.ViewSet):
-    def list(self, request, organization):
-        queryset = Inventory.objects.for_organization(organization)
-        serializer = InventorySerializer(queryset, many=True)
-        return Response(serializer.data)
+    def patch(self, request, pk, organization):
+        """
+        Update a single location by ID
+        """
+        queryset = Location.objects.for_organization(organization)
+        location = get_object_or_404(Location, pk=pk)
+        serializer = self.get_serializer(location, data=request.data)
+        return save_serializer_with_organization(serializer, organization)
 
-    def retrieve(self, request, organization, pk=None):
-        queryset = Inventory.objects.for_organization(organization)
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = InventorySerializer(user)
-        return Response(serializer.data)
+    def delete(self, request, pk, organization):
+        """
+        Delete a single location by ID
+        """
+        queryset = Location.objects.for_organization(organization)
+        location = get_object_or_404(queryset, pk=pk)
+        location.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
 
 
-class MutationViewSet(viewsets.ViewSet):
-    def list(self, request, organization):
-        queryset = Mutation.objects.for_organization(organization)
-        serializer = MutationSerializer(queryset, many=True)
-        return Response(serializer.data)
+# # class InventoryViewSet(viewsets.ViewSet):
+# #     """
+# #     list:
+# #     A list of the inventory of the organization
 
-    def retrieve(self, request, organization, pk=None):
-        queryset = Mutation.objects.for_organization(organization)
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = MutationSerializer(user)
-        return Response(serializer.data)
+# #     retrieve:
+# #     Get a detailed overview of a single inventory (location and product specific)
+# #     """
+
+# #     def list(self, request, organization):
+# #         queryset = Inventory.objects.for_organization(organization)
+# #         serializer = InventorySerializer(queryset, many=True)
+# #         return Response(serializer.data)
+
+# #     def retrieve(self, request, organization, pk=None):
+# #         queryset = Inventory.objects.for_organization(organization)
+# #         inventory = get_object_or_404(queryset, pk=pk)
+# #         serializer = InventorySerializer(inventory)
+# #         return Response(serializer.data)
+
+
+# # class MutationViewSet(viewsets.ViewSet):
+# #     """
+# #     list:
+# #     All the mutations of the organization
+
+# #     retrieve:
+# #     Get a single mutation and its details
+# #     """
+
+# #     def list(self, request, organization):
+# #         queryset = Mutation.objects.for_organization(organization)
+# #         serializer = MutationSerializer(queryset, many=True)
+# #         return Response(serializer.data)
+
+# #     def retrieve(self, request, organization, pk=None):
+# #         queryset = Mutation.objects.for_organization(organization)
+# #         mutation = get_object_or_404(queryset, pk=pk)
+# #         serializer = MutationSerializer(mutation)
+# #         return Response(serializer.data)
--- docs.sh
+++ docs.sh
@@ -1,5 +1,5 @@
 # Generating API docs
-python manage.py generateschema --urlconf inventory.urls --title "Organization.supply REST API" > docs/api.yml
+python manage.py generateschema --urlconf inventory.urls --description="Documentation for the REST API for Organization.supply" --title="Organization.supply REST API" > docs/api.yml
 cp docs/api.yml hugo/content/api.yml # Also copy it to the static site content folder 
 
 # Generating static site
