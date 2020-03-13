af9203e2b1fff0dfeabb4517df5efc0c7b0db9a5
Joost Plattel <jsplattel@gmail.com>
2020-03-13 19:13:29 +0100

    added report page

--- reports/views.py
+++ reports/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.db.models import F, Func, Q, Sum, Window
 from django.shortcuts import redirect, render
 from organization.models.inventory import Product, Location, Mutation
 from django.db.models import Sum, Value
@@ -7,7 +8,21 @@ from django.db.models import Sum, Value
 @login_required
 def reports_index(request):
     products = Product.objects.for_organization(request.organization)
+    product_mutations = {}
+    for product in products:
+        product_mutations[product.name] = (
+            Mutation.objects.for_organization(request.organization)
+            .filter(
+                product=product,
+                contra_mutation__isnull=True,
+                operation__in=["add", "remove"],
+            )
+            .annotate(cumsum=Window(Sum("amount"), order_by=F("created").asc()))
+            .values("id", "cumsum", "amount", "desc", "created")
+            .order_by("-created")
+        )
     return render(request, "reports/index.html", {
+        "product_mutations": product_mutations,
         "counts": {
             "products": products.count(),
             "locations": Location.objects.for_organization(request.organization).count(),
